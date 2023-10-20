b1569f11daf617c7ad22846acaa3a6618b18ad42
azixkhan <azixkhan.55@gmail.com>
2023-10-20 11:35:14 +0500

    convert lat and long to address

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -28,6 +28,26 @@ class _HomeScreenState extends State<HomeScreen> {
       position: LatLng(33.720000, 73.060000),
       infoWindow: InfoWindow(title: 'Deans'),
     ),
+    Marker(
+      markerId: MarkerId('3'),
+      position: LatLng(36.2048, 138.2529),
+      infoWindow: InfoWindow(title: 'Japan'),
+    ),
+    Marker(
+      markerId: MarkerId('4'),
+      position: LatLng(38.9637, 35.2433),
+      infoWindow: InfoWindow(title: 'Turkey'),
+    ),
+    Marker(
+      markerId: MarkerId('5'),
+      position: LatLng(61.5240, 105.3188),
+      infoWindow: InfoWindow(title: 'Russia'),
+    ),
+    Marker(
+      markerId: MarkerId('6'),
+      position: LatLng(40.741895, -73.989308),
+      infoWindow: InfoWindow(title: 'New York'),
+    ),
   ];
 
   @override
@@ -39,16 +59,30 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        onMapCreated: (GoogleMapController controller) {
-          _completer.complete(controller);
-        },
-        initialCameraPosition: _kGooglePlex,
-        markers: Set<Marker>.of(_marker),
-        mapType: MapType.normal,
-        compassEnabled: true,
-        myLocationEnabled: true,
+      body: SafeArea(
+        child: GoogleMap(
+          onMapCreated: (GoogleMapController controller) {
+            _completer.complete(controller);
+          },
+          initialCameraPosition: _kGooglePlex,
+          markers: Set<Marker>.of(_marker),
+          mapType: MapType.normal,
+          compassEnabled: true,
+          myLocationEnabled: true,
+        ),
       ),
+      floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.my_location),
+          onPressed: () async {
+            GoogleMapController controller = await _completer.future;
+            controller.animateCamera(
+              CameraUpdate.newCameraPosition(
+                const CameraPosition(
+                    target: LatLng(40.741895, -73.989308), zoom: 14),
+              ),
+            );
+            setState(() {});
+          }),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps/convert_latlang_to_address.dart';
 import 'package:google_maps/home_screen.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: const ConvertLatLangToAddress(),
     );
   }
 }
