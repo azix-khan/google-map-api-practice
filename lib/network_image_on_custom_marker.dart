e07147f98e501df0f72fee24a949e91d12dbc083
azixkhan <azixkhan.55@gmail.com>
2023-10-28 11:07:52 +0500

    make custom network image(bitmoji) on markers

--- lib/network_image_on_custom_marker.dart
+++ lib/network_image_on_custom_marker.dart
@@ -1,8 +1,6 @@
 import 'dart:async';
 import 'dart:ui';
 import 'dart:ui' as ui;
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
